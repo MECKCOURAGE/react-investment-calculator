b804518c0668711fa179000ec59d06945a6692a3
courage meck <theme@DESKTOP-T1QIHLE.Prompt>
2024-10-03 18:19:29 +0100

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header () {
+    return (
+        <header id="header">
+            <img src="download.png" alt="Investment logo" />
+            <h1>Investment Calculator</h1>
+        </header>
+    )
+
+}
